33d1406cd47289d97e8bce9330ccaf0829b9d5c1
watson <276322154@qq.com>
2025-09-27 20:00:00 +0900

    Initial Commit

---
+++ ShoppingCartCucumberish/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingCartCucumberish
+//
+//  Created by 沃霊宗 on 2025/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ShoppingCartCucumberishTests/ShoppingCartCucumberishTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCartCucumberishTests.swift
+//  ShoppingCartCucumberishTests
+//
+//  Created by 沃霊宗 on 2025/09/27.
+//
+
+import Testing
+@testable import ShoppingCartCucumberish
+
+struct ShoppingCartCucumberishTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
